fd494adb62e5c319dee5ca9b9f8c74cdfea42fa9
warmjade <sztaoyong@gmail.com>
2014-03-28 01:53:47 -0700

    wrap all into one func

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,57 @@
 var caseId = 100000000;
 var associateId = 1999;
+var timer;
+var tick;
+var active;
 
-function setStartTime(){
-    if (!supportsLocalStorage()) {
-        alert("local storage failed!"); 
-        return false; 
-    }
-    var startTime = new Date();
-    var key = "CST" + caseId; // Case start time
-    var values = {csId:associateId, start:startTime};
-    localStorage[key] = JSON.stringify(values);
+function startTimer(){
+    loadTimer();
+    active = true;
+    tick = setInterval(incrementTimer, 1000);
     return true;
 }
 
-function computeAHT(){
-    if (!supportsLocalStorage()) { 
-        alert("local storage failed!");
-        return false; 
+function loadTimer(){
+    if(localStorage.getItem("lastTimer" + caseId) === null){
+        timer = 0;
+    }else{
+        timer = localStorage.getItem("lastTimer" + caseId);
     }
-    var key = "CST" + caseId;
-    if (localStorage.getItem(key) === null) {
-        alert("this case has been solved. refresh the page to get next case!");
-        return false;
-    } else{
-        var startValues = JSON.parse(localStorage[key]);
-        var associateId = startValues.csId;
-        var startTime = new Date(startValues.start);
-        var endTime = new Date();
-        var caseHandleTime = endTime - startTime;
+}
+
+window.onblur = function() {
+    active = false;
+};
+
+window.onfocus = function() {
+    active = true;
+};
+
+window.onunload = window.onbeforeunload = function(){
+    key = "lastTimer" + caseId;
+    localStorage[key] = timer;
+};
+
+function incrementTimer(){
+    if (active) {
+        timer++;
+    }
+    document.getElementById("timer").innerHTML = secondToTime(timer);
+}
+
+function computeAHT(){
+    if (supportsLocalStorage()) { 
+        var caseHandleTime = timer;
         localStorage["lastCHT" + associateId] = caseHandleTime; // save this case handle time;
-        if (!updateAHT(associateId, caseHandleTime)) { 
-            return false; 
-        }else{
-            localStorage.removeItem(key); // this case has been resolved;
-            alert(JSON.stringify(displayInfo())); // display AHT information;
+        if (updateAHT(associateId, caseHandleTime)) { 
+            alert(JSON.stringify(displayInfo())); // display AHT information; // all pass parameters
+            clearInterval(tick);
+            localStorage.removeItem("lastTimer" + caseId);
+            timer = 0;
             return true;
         }
+    }else{
+        // alert('local storage failed!');
     }
 }
 
@@ -47,17 +63,16 @@ function displayInfo(){
      var lastCHTValue = localStorage["lastCHT" + associateId];
      var AHTValues = JSON.parse(localStorage["AHT" + associateId]);
 
-     var AHTInfo = {lastCaseHandleTime:msToTime(lastCHTValue), AHT:msToTime(AHTValues.AHT)};
+     var AHTInfo = {lastCaseHandleTime:secondToTime(lastCHTValue), AHT:secondToTime(AHTValues.AHT)};
      return AHTInfo;
 }
 
-function msToTime(s) {
+function secondToTime(ss) {
     function padding(n) {
         return (n<10? '0':'') + n;
     }
     
-    var ms = s % 1000;
-    s = (s - ms) / 1000;
+    var s = Math.round(ss);
     var secs = s % 60;
     s = (s - secs) / 60;
     var mins = s % 60;
